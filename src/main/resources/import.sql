c5aad3fa8565c356d4dd577edd3250f85a59f13b
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-06-08 20:50:25 -0300

    add course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://foundr.com/wp-content/uploads/2023/04/How-to-create-an-online-course.jpg.webp', 'https://i.ytimg.com/vi/0WV1W-0004U/maxresdefault.jpg');
