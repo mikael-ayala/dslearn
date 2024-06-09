2887a78bfbb627df00c136afbd1d84ee8242ce62
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-06-08 21:09:09 -0300

    add Offer

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://foundr.com/wp-content/uploads/2023/04/How-to-create-an-online-course.jpg.webp', 'https://i.ytimg.com/vi/0WV1W-0004U/maxresdefault.jpg');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00', 1);
